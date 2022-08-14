3ea03bc06012869103a30a51a7818c28cc1e55c6
tjubigben <2605121231@qq.com>
2022-08-14 17:26:10 +0800

    数组

---
+++ src/com/company/array/FindMaxConsecutiveOnes.java
@@ -0,0 +1,37 @@
+package com.company.array;
+
+/**
+ * 485 <br>
+ * 链接：https://leetcode.cn/problems/max-consecutive-ones/ <br>
+ * 给定一个二进制数组 nums ， 计算其中最大连续 1 的个数。
+ *
+ * @author bigben
+ */
+public class FindMaxConsecutiveOnes {
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 1, 0, 1, 1, 1};
+        int maxConsecutiveOnesLength = findMaxConsecutiveOnes(nums);
+        System.out.println(maxConsecutiveOnesLength);
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int consecutiveOnesLength = 0;
+        int maxConsecutiveOnesLength = 0;
+        for (int num : nums) {
+            if ((num & 1) > 0) {
+                ++consecutiveOnesLength;
+            } else {
+                if (consecutiveOnesLength > maxConsecutiveOnesLength) {
+                    maxConsecutiveOnesLength = consecutiveOnesLength;
+                }
+                consecutiveOnesLength = 0;
+            }
+        }
+        return Math.max(maxConsecutiveOnesLength, consecutiveOnesLength);
+    }
+}
---
+++ src/com/company/array/FindPoisonedDuration.java
@@ -0,0 +1,35 @@
+package com.company.array;
+
+/**
+ * 495 <br>
+ * 链接：https://leetcode.cn/problems/teemo-attacking/ <br>
+ * 在《英雄联盟》的世界中，有一个叫 “提莫” 的英雄。他的攻击可以让敌方英雄艾希（编者注：寒冰射手）进入中毒状态。<br>
+ * 当提莫攻击艾希，艾希的中毒状态正好持续 duration 秒。<br>
+ * 正式地讲，提莫在 t 发起发起攻击意味着艾希在时间区间 [t, t + duration - 1]（含 t 和 t + duration - 1）处于中毒状态。
+ * 如果提莫在中毒影响结束 前 再次攻击，中毒状态计时器将会 重置 ，在新的攻击之后，中毒影响将会在 duration 秒后结束。 <br>
+ * 给你一个 非递减 的整数数组 timeSeries ，其中 timeSeries[i] 表示提莫在 timeSeries[i] 秒时对艾希发起攻击，以及一个表示中毒持续时间的整数 duration 。 <br>
+ * 返回艾希处于中毒状态的 总 秒数 <br>
+ *
+ * @author bigben
+ */
+public class FindPoisonedDuration {
+    public static int findPoisonedDuration(int[] timeSeries, int duration) {
+        int poisonedDurationTime = 0;
+        int expiredPoisonedDurationTime = 0;
+        for (int time : timeSeries) {
+            if (time > expiredPoisonedDurationTime) {
+                poisonedDurationTime += duration;
+            } else {
+                poisonedDurationTime += time + duration - expiredPoisonedDurationTime;
+            }
+            expiredPoisonedDurationTime = time + duration;
+        }
+        return poisonedDurationTime;
+    }
+
+    public static void main(String[] args) {
+        int[] timeSeries = new int[]{3, 6};
+        int result = findPoisonedDuration(timeSeries, 7);
+        System.out.println(result);
+    }
+}
